474b1162f73045836dfa64a99152fe41f19e0281
Tim Broughton <TimBroughton@quickenloans.com>
2019-01-02 16:12:47 -0500

    updated how data was applied to model and passed to view

--- BoredWebApp/Controllers/OneController.cs
+++ BoredWebApp/Controllers/OneController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BoredWebApp.Models;
 
 namespace BoredWebApp.Controllers
 {
@@ -15,7 +16,7 @@ namespace BoredWebApp.Controllers
 
         const string userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
 
-        public ActionResult Index()
+        public  ActionResult Index()
         {
             HttpWebRequest request = WebRequest.CreateHttp("https://www.boredapi.com/api/Activity");
 
@@ -23,6 +24,9 @@ namespace BoredWebApp.Controllers
 
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //model insert
+            Activity NewActivity = new Activity();
 
 
             if (response.StatusCode == HttpStatusCode.OK)
@@ -32,12 +36,18 @@ namespace BoredWebApp.Controllers
 
                 string JsonData = data.ReadToEnd();
 
-                JObject dataObject = JObject.Parse("{activities:" + JsonData + "}");
+                JObject dataObject = JObject.Parse(JsonData);
 
-                ViewBag.Activities = dataObject;
+                ViewBag.Activity = dataObject.ToObject<Activity>();
+
+                
+                return View();
+            }
+            else
+            {
+                return null;
             }
 
-            return View();
         }
         
     }
