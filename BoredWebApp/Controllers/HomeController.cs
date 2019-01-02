4b533a9c5b75f9125e4fbadcae9f968df9a6252c
Madelyn Hilty <madelynhilty@quickenloans.com>
2019-01-02 15:07:47 -0500

    copying old project over

---
+++ BoredWebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoredWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult One()
+        {
+            return RedirectToAction("Index", "One");
+        }
+
+        public ActionResult Specific()
+        {
+
+            return RedirectToAction("Index", "Specific");
+        }
+
+        public ActionResult Multiple()
+        {
+
+            return RedirectToAction("Index", "Multiple");
+        }
+    }
+}
---
+++ BoredWebApp/Controllers/OneController.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoredWebApp.Controllers
+{
+    public class OneController : Controller
+    {
+
+
+        const string userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+        public ActionResult Index()
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("https://www.boredapi.com/api/Activity");
+
+            request.UserAgent = userAgent;
+
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+
+                StreamReader data = new StreamReader(response.GetResponseStream());
+
+                string JsonData = data.ReadToEnd();
+
+                JObject dataObject = JObject.Parse("{activities:" + JsonData + "}");
+
+                ViewBag.Activities = dataObject;
+            }
+
+            return View();
+        }
+        
+    }
+}
